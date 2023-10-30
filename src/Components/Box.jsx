6d1ef017a52f717a8a5ec9c9d5c61962ceb278c7
Liam Wedge <21100218@virtualwindow.co.za>
2023-10-30 18:06:14 +0200

    ui updates for p2

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
-export default function Box() {
-  return <div>Box</div>;
+import "./Box.scss";
+export default function Box(card) {
+  return (
+    <div className="infoBlock">
+      <div>
+        <img src={card.image} className="boxImg" />
+        <h5>{card.info}</h5>
+      </div>
+    </div>
+  );
 }
--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -1,15 +1,29 @@
 import KommunicateBot from "../../Components/KommunicateBot";
 import "./Dashboard.scss";
 import homeIcon from "../../Assets/illustrations/loving.png";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Box from "../../Components/Box";
+import dog from "../../Assets/illustrations/dog-jump.png";
+import plant from "../../Assets/illustrations/plant.png";
+import sprinting from "../../Assets/illustrations/sprinting.png";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="dashCon">
       <div className="header">
-        <h1>Kinder Care</h1>
-        <h3>Educating your kids the right way</h3>
+        <div className="info">
+          <h1>Kinder Care</h1>
+          <h3>Educating your kids through AI </h3>
+          <p>
+            Through the power of artificial intelligence, <br /> Kindercare aims
+            to help grow your kids knowledge
+          </p>
+          <Box image={dog} info={"Get help"} />
+
+          <Box image={plant} info={"Ask questions "} />
+          <Box image={sprinting} info={"Play games"} />
+        </div>
+
         <img className="homeimg" src={homeIcon} />
       </div>
       <KommunicateBot />
